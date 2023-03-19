889f4bed8491e2d8a510379dd5cd3924c46c0bc1
Colin Campbell <prontobol@gmail.com>
2023-03-19 12:11:39 -0500

    Added text field

--- lib/user/pages/group/group_confidants.dart
+++ lib/user/pages/group/group_confidants.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:safezone_frontend/providers/providers.dart';
 import 'package:safezone_frontend/user/pages/group/confidant_card.dart';
 import 'package:safezone_frontend/widgets/app_bar.dart';
+import 'package:safezone_frontend/widgets/app_text_field.dart';
 
 class GroupConfidants extends ConsumerStatefulWidget {
   static final String routeName = "/group_confidant";
@@ -31,6 +32,10 @@ class _GroupConfidantsState extends ConsumerState<GroupConfidants> {
       body: Column(
         children: [
           CustomAppBar(title: "My Confidants"),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: AppTextField(hintText: "Search", onSaved: (val) {}),
+          ),
           Expanded(
               child: ListView.builder(
                   itemCount: group.confidants.length,
