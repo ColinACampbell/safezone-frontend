507e050ff0b43ae65ac390ec2672423d0dec7232
Colin Campbell <colincampbell@Colins-MacBook-Pro.local>
2023-05-12 13:07:59 -0500

    Implemented marker selection & fob

--- lib/user/pages/group/add_geo_fence.dart
+++ lib/user/pages/group/add_geo_fence.dart
@@ -20,29 +20,20 @@ class _AddGeoFenceScreenState extends ConsumerState {
   double pointWidth = 10;
   double scaledWidth = 0;
   double zoomLevel = 0;
+  bool isCirclePresent = false; // todo: Implement
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     setState(() {
       scaledWidth = calcScaledWidth(pointWidth, 17);
-      print(scaledWidth);
       zoomLevel = 17;
     });
   }
 
+  // Scales the width of the marker on the map based of the zoom level
   double calcScaledWidth(double pWidth, double scale) {
     return (scale * pWidth) / 17;
-  }
-
-  double metersPerPixel(double latitude, double zoom) {
-     //return (156543.03392 *
-     //    math.cos(latitude * math.pi / 180) /
-     //    math.pow(3,zoom));
-    return 40075016.686 * (math.cos(latitude * math.pi/180)) / math.pow(2,zoom+8).abs();
-
-
   }
 
   double getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
@@ -72,7 +63,6 @@ class _AddGeoFenceScreenState extends ConsumerState {
           return Container(
             decoration: const BoxDecoration(
               color: Color.fromRGBO(255, 0, 0, .9),
-              //borderRadius: BorderRadius.all(Radius.circular(100))
             ),
           );
         });
@@ -90,92 +80,108 @@ class _AddGeoFenceScreenState extends ConsumerState {
         builder: (context) {
           return Container(
             decoration: const BoxDecoration(
-                color: Color.fromRGBO(255, 0, 0, .9),
-                shape: BoxShape.circle
-                ),
+                color: Color.fromRGBO(255, 0, 0, .9), shape: BoxShape.circle),
           );
         });
   }
 
-  void lat_from_distance(double latitude, distance) {
-    var earth = 6378.137, //radius of the earth in kilometer
-        pi = math.pi,
-        m = (1 / ((2 * pi / 360) * earth)) / 1000; //1 meter in degree
+  // TODO: Remove
+  // void lat_from_distance(double latitude, distance) {
+  //   var earth = 6378.137, //radius of the earth in kilometer
+  //       pi = math.pi,
+  //       m = (1 / ((2 * pi / 360) * earth)) / 1000; //1 meter in degree
 
-    var new_latitude = latitude + (distance * m);
-  }
+  //   var new_latitude = latitude + (distance * m);
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        alignment: Alignment.bottomCenter,
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          FlutterMap(
-            options: MapOptions(
-              onMapCreated: (controlelr) {},
-              onPositionChanged: ((position, hasGesture) {
-                zoomLevel = position.zoom!;
-                if (geoFenceMarkers.isNotEmpty) {
-                  setState(() {
-                    scaledWidth = calcScaledWidth(pointWidth, position.zoom!);
+          FloatingActionButton(
+          onPressed: () {
+            if (geoFenceMarkers.length == 2) {
+              Marker m1 = geoFenceMarkers[0];
+              Marker m2 = geoFenceMarkers[1];
 
-                    for (int i = 0; i < geoFenceMarkers.length; i++) {
-                      var oldMarker = geoFenceMarkers[i];
-                      geoFenceMarkers[i] =
-                          createMarker(oldMarker.point, scaledWidth);
-                    }
-
-                    //Marker oldM = geoFenceMarkers.removeAt(0);
-                    //addMarker(oldM.point, scaledWidth);
-                  });
-                }
-              }),
-              onTap: (p, l) async {
-                if (geoFenceMarkers.length < 2) {
-                  addMarker(l, scaledWidth);
-                } else if (geoFenceMarkers.length == 2) {
-                  geoFenceMarkers.removeLast();
-                  addMarker(l, scaledWidth);
-                }
-              },
-              center: LatLng(17.898418, -76.906672),
-              zoom: 17.0,
-              maxZoom: 17,
-            ),
-            layers: [
-              TileLayerOptions(
-                  urlTemplate:
-                      "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-                  subdomains: ['a', 'b', 'c']),
-              MarkerLayerOptions(markers: geoFenceMarkers),
-              CircleLayerOptions(circles: circleMarkers)
-            ],
-          ),
-          ElevatedButton(
-              onPressed: () {
-                if (geoFenceMarkers.length == 2) {
-                  Marker m1 = geoFenceMarkers[0];
-                  Marker m2 = geoFenceMarkers[1];
-
-                  double distance = getDistanceFromLatLonInKm(
+              double distanceInMeters = getDistanceFromLatLonInKm(
                       m1.point.latitude,
                       m1.point.longitude,
                       m2.point.latitude,
-                      m2.point.longitude) * 1000; 
+                      m2.point.longitude) *
+                  1000;
 
-                  // print("Zoom level is $zoomLevel");
-                  // final ratio = metersPerPixel(m1.point.latitude, zoomLevel);
-                  // print("Ratio is $ratio");
-                  // print("Distance is $distance");
-                  // print("Pixel size is ${(distance) / ratio}");
-                  // setState(() {
-                  //   geoFenceMarkers[0] =
-                  //       createMarker(m1.point, (distance * ratio) + (200 * ratio)  );
-                  // });
+              setState(() {
+                var newCircle = CircleMarker(
+                    point: m1.point,
+                    radius: distanceInMeters,
+                    useRadiusInMeter: true,
+                    color: const Color.fromRGBO(200, 200, 200, .5));
+                if (circleMarkers.length == 0) {
+                  circleMarkers.add(newCircle);
+                } else {
+                  circleMarkers[0] = newCircle;
                 }
-              },
-              child: const Text("Hello World"))
+              });
+            }
+          },
+          child: const Icon(Icons.add)),
+          const SizedBox(height: 20),
+          FloatingActionButton(onPressed: (){}, child: const Icon(Icons.delete),)
+        ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Stack(
+              children: [
+                FlutterMap(
+                  options: MapOptions(
+                    onMapCreated: (controlelr) {},
+                    onPositionChanged: ((position, hasGesture) {
+                      zoomLevel = position.zoom!;
+                      if (geoFenceMarkers.isNotEmpty) {
+                        setState(() {
+                          scaledWidth = calcScaledWidth(
+                              pointWidth,
+                              position
+                                  .zoom!); // anytime the zoom changes we want to change the ratio of the zoom on the screen
+
+                          for (int i = 0; i < geoFenceMarkers.length; i++) {
+                            var oldMarker = geoFenceMarkers[i];
+                            geoFenceMarkers[i] =
+                                createMarker(oldMarker.point, scaledWidth);
+                          }
+                        });
+                      }
+                    }),
+                    onTap: (p, l) async {
+                      if (geoFenceMarkers.length < 2) {
+                        addMarker(l, scaledWidth);
+                      } else if (geoFenceMarkers.length == 2) {
+                        geoFenceMarkers.removeLast();
+                        addMarker(l, scaledWidth);
+                      }
+                    },
+                    center: LatLng(17.898418, -76.906672),
+                    zoom: 17.0,
+                    maxZoom: 17,
+                  ),
+                  layers: [
+                    TileLayerOptions(
+                        urlTemplate:
+                            "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                        subdomains: ['a', 'b', 'c']),
+                    MarkerLayerOptions(markers: geoFenceMarkers),
+                    CircleLayerOptions(circles: circleMarkers)
+                  ],
+                )
+              ],
+            ),
+          )
         ],
       ),
     );
